c40a1cec9e2eb25e9ae60e23326c98c0fbd9a0bd
ojo29 <ojo29@uclive.ac.nz>
2025-07-22 22:32:32 +1000

    implimented engine object

--- Pi_Main/game/player.py
+++ Pi_Main/game/player.py
@@ -1,14 +1,14 @@
 import agents
 
 class Player:
-    def __init__(self, colour, playerType='human'):
+    def __init__(self, colour, engine, playerType='human'):
         self.colour = colour # colour is 'black' or 'white'
         self.playerType = playerType
 
         if playerType == 'human':
-            self.agent = agents.Human()
+            self.agent = agents.Human(engine, bitmap_container, 20)
         elif playerType == 'bot':
-            self.agent = agents.Bot()
+            self.agent = agents.Bot(engine, bitmap_container, 5)
 
 
         
